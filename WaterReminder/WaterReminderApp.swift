2ecdfc4755c7820df8c38f5dc6682c96550af353
veronicazhao <zz2906@nyu.edu>
2025-01-10 15:22:57 -0500

    Initial Commit

---
+++ WaterReminder/WaterReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterReminderApp.swift
+//  WaterReminder
+//
+//  Created by test on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
